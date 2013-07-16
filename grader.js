a0c209ff8a9e6cf75766ef074ae2b0f7109785e0
superschedulerapp <superschedulerapp@gmail.com>
2013-07-16 01:22:30 -0400

    modified:   checks.json 	modified:   grader.js 	modified:   index.html 	modified:   web.js 	notes.org

--- grader.js
+++ grader.js
@@ -26,6 +26,8 @@ var program = require('commander');
 var cheerio = require('cheerio');
 var HTMLFILE_DEFAULT = "index.html";
 var CHECKSFILE_DEFAULT = "checks.json";
+var ADDY_DEFAULT = "http://radiant-temple-2056.herokuapp.com/";
+var restler = require("restler");
 
 var assertFileExists = function(infile) {
     var instr = infile.toString();
@@ -35,6 +37,12 @@ var assertFileExists = function(infile) {
     }
     return instr;
 };
+
+
+var downLoadFile = function(inAddress) {
+    var page= restler.get(inAddress);
+    return page.toString();
+};    
 
 var cheerioHtmlFile = function(htmlfile) {
     return cheerio.load(fs.readFileSync(htmlfile));
@@ -65,6 +73,7 @@ if(require.main == module) {
     program
         .option('-c, --checks <check_file>', 'Path to checks.json', clone(assertFileExists), CHECKSFILE_DEFAULT)
         .option('-f, --file <html_file>', 'Path to index.html', clone(assertFileExists), HTMLFILE_DEFAULT)
+        .option('-u, --url <html file>', 'Path to index.html', clone(downLoadFile), HTMLFILE_DEFAULT)
         .parse(process.argv);
     var checkJson = checkHtmlFile(program.file, program.checks);
     var outJson = JSON.stringify(checkJson, null, 4);
